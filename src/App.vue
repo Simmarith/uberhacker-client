9aa7d9b30dfb83e188fe7afa69e75317b8e35fed
amenzel <adrian.menzel@voltstorage.com>
2018-08-05 23:03:31 +0200

    recreate with proper modules

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,7 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app',
-  components: {
-  }
-}
-</script>
-
-<style>
+<style lang="less">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
