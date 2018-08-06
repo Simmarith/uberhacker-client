de85e4507b384095bbba9f9a5848e854c200cab8
amenzel <adrian.menzel@voltstorage.com>
2018-08-06 08:07:02 +0200

    wrap login with window

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,27 @@
 <template>
-  <div class="login">
-    Username:
-    <input v-model="username" />
-    <br />
-    <button v-on:click="login">Login</button>
-  </div>
+   <Window
+    header="LOGIN"
+    width="200px"
+    height="200px"
+    top="calc(50vh - 100px)"
+    left="calc(50vw - 100px)"
+  >
+    <div class="login">
+      <span class="username-label">Username:</span>
+      <input v-model="username" />
+      <br />
+      <button class="login-submit" v-on:click="login">Login</button>
+    </div>
+  </Window>
 </template>
 
 <script>
+import Window from '@/components/Window.vue'
+
 export default {
   name: 'login',
+  components: { Window },
+  data: function () { return { username: '' } },
   methods: {
     login: function () {
       this.$router.push({ name: 'desktop', params: { username: this.username } })
@@ -17,3 +29,21 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="less">
+  .login {
+    * {
+      margin: 5px;
+    }
+
+    .username-label {
+      display: block;
+    }
+
+    .login-submit {
+      position: absolute;
+      bottom: 0px;
+      right: 0px;
+    }
+  }
+</style>
