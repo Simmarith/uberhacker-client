aa55f747ccbc568278b2b2d83363a18d70cb5842
amenzel <adrian.menzel@voltstorage.com>
2018-08-14 08:41:18 +0200

    User Window
    all users online on this server are now displayed in the Users component

---
+++ src/components/Users.vue
@@ -0,0 +1,28 @@
+<template>
+  <Window
+  header="USERS"
+  top="0"
+  left="0"
+  width="15vw"
+  height="10vh"
+  >
+  <div :key="user.id" v-for="user in users">
+    {{user.id}}: {{user.points}}pts
+  </div>
+  </Window>
+</template>
+
+<script>
+import Window from '@/components/Window'
+import UserStore from '@/stores/UserStore'
+
+export default {
+  name: 'Users',
+  components: { Window },
+  computed: {
+    users () {
+      return UserStore.users
+    }
+  }
+}
+</script>
--- src/views/Desktop.vue
+++ src/views/Desktop.vue
@@ -1,32 +1,25 @@
 <template>
   <div class="desktop">
-   <Memes />
     <Memes />
+    <Memes />
+    <Users />
     <Window
-      header="GREETING"
-      width="200px"
-      height="200px"
-      top="calc(50vh - 100px)"
-      left="calc(50vw - 100px)"
-    >
-      <span class="greeting">Welcome, {{ $route.params.username }}!</span>
-    </Window>
-    <Window
-      header="SECOND WINDOW"
-      height="150px"
-      width="300px"
-      top="50px"
-      left="50px"
-    >
-    Hello!
+       header="GREETING"
+       width="200px"
+       height="200px"
+       top="calc(50vh - 100px)"
+       left="calc(50vw - 100px)"
+       >
+       <span class="greeting">Welcome, {{ $route.params.username }}!</span>
     </Window>
   </div>
 </template>
 
 <script>
-import Window from '@/components/Window.vue'
-import Memes from '@/components/Memes.vue'
+import Window from '@/components/Window'
+import Memes from '@/components/Memes'
 import UserStore from '@/stores/UserStore'
+import Users from '@/components/Users'
 
 export default {
   name: 'desktop',
@@ -35,7 +28,8 @@ export default {
   },
   components: {
     Window,
-    Memes
+    Memes,
+    Users
   },
   computed: {
     servername () {
